bd82a42a443be883612eb8872d27ddc495c89f73
Luk <lucajin551@gmail.com>
2023-10-09 10:22:55 +0200

    Main.java modified

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public static void Main(String[]args){
 	String texto =b "heheh";
 	System.outPrintln("Hola mundo");
 	System.outPrintln(texto);
+	System.outPrintln("gigi")
 	System.out.Println("adios");
 }
