e0459eeeadbfdf413a2c6c281b9775243a80f427
Luk <lucajin551@gmail.com>
2023-10-09 10:30:35 +0200

    Main.java added

--- Main.java
+++ Main.java
@@ -4,4 +4,11 @@ public static void Main(String[]args){
 	System.outPrintln(texto);
 	System.outPrintln("gigi")
 	System.out.Println("adios");
+
+	int num1 = 1;
+	int num2 = 2;
+	System.out.println(addNumbers(num1,num2));
+}
+public static int addnumbers(int a , int b){
+	return (a+b);
 }
